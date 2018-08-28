4c8e1d71de503dd9ad6691fcbe4b6472c1456704
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-08-28 21:33:48 +0800

    added a welcome message

--- sedit.c
+++ sedit.c
@@ -12,6 +12,8 @@
 
 /*** defines ***/
 
+#define SEDIT_VERSION "0.0.1"
+
 #define CTRL_KEY(k)     ((k) & 0x1f)
 
 
@@ -128,7 +130,21 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; ++y) {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+            "sedit text editor -- version %s", SEDIT_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
@@ -141,7 +157,7 @@ void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);   // hide the cursor before rendering anything
-    
+
     abAppend(&ab, "\x1b[H", 3);      // Reposition the cursor at the beginning
 
     editorDrawRows(&ab);
