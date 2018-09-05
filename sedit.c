1b8cdc5bec17767d52c5e00a92f1a50dd24ddfec
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-09-05 22:31:47 +0800

    Added vertical scrolling to allow scrolling through an entire file

--- sedit.c
+++ sedit.c
@@ -43,6 +43,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;                             // row offset
     int screenrows;
     int screencols;
     int numrows;
@@ -226,10 +227,20 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+    if (E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; ++y) {
-        if (y >= E.numrows) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -246,9 +257,9 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -259,6 +270,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);   // hide the cursor before rendering anything
@@ -268,7 +281,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);   // show the cursor again after refreshing the screen
@@ -292,7 +305,7 @@ void editorMoveCursor(int key) {
             if (E.cy > 0) E.cy--;
             break;
         case ARROW_DOWN:
-            if (E.cy < E.screenrows) E.cy++;
+            if (E.cy < E.numrows) E.cy++;
             break;
         case PAGE_UP:
             E.cy = 0;
@@ -338,6 +351,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
 
