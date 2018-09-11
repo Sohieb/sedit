27f7ae48112522f5067193eb92f8d6a2ec2ab40e
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-09-12 01:39:35 +0800

    Save abortion when pressing ESC while writing the save-as file name

--- sedit.c
+++ sedit.c
@@ -371,7 +371,11 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
     }
 
     int len;
@@ -550,7 +554,11 @@ char *editorPrompt(char *prompt) {
         editorRefreshScreen();
 
         int c = editorReadKey();
-        if (c == '\r') {
+        if (c == '\x1b') {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        } else if (c == '\r') {
             if (buflen != 0) {
                 editorSetStatusMessage("");
                 return buf;
