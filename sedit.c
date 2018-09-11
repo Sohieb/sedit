17729ee8f2369f41524181550f7629dfd522ce0f
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-09-11 18:02:40 +0800

    save the current edited file when pressing CTRL-s

--- sedit.c
+++ sedit.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -255,6 +256,25 @@ void editorInsertChar(int c) {
 
 /*** file i/o ***/
 
+char *editorRowToString(int *buflen) {
+    int totlen = 0;
+    int j;
+    for (j = 0; j < E.numrows; ++j)
+        totlen += E.row[j].size + 1;
+    *buflen = totlen;
+
+    char *buf = malloc(totlen);
+    char *p = buf;
+    for (j = 0; j < E.numrows; ++j) {
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+
+    return buf;
+}
+
 void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
@@ -272,6 +292,19 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave() {
+    if (E.filename == NULL) return;
+
+    int len;
+    char *buf = editorRowToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    ftruncate(fd, len);
+    write(fd, buf, len);
+    close(fd);
+    free(buf);
 }
 
 
@@ -482,6 +515,10 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);     // clear the entire screen
             write(STDOUT_FILENO, "\x1b[H", 3);      // Reposition the cursor at the beginning
             exit(0);
+            break;
+
+        case CTRL_KEY('s'):
+            editorSave();
             break;
         
         case BACKSPACE:
